eda77ed2a1f2245b7597aa039d3729eb0f699860
Pavlo Dzvonyk <reptile1601@yahoo.com>
2023-11-25 09:06:31 +0100

    created kivy boilerplate

---
+++ App-4-Webcam-Photo_Sharer/main.py
@@ -0,0 +1,25 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+
+Builder.load_file("frontend.kv")
+
+
+class CameraScreen(Screen):  # (requirement from kivy)
+    pass
+
+
+class ImageScreen(Screen):  # (requirement from kivy)
+    pass
+
+
+class RootWidget(ScreenManager):
+    pass
+
+
+class MainApp(App):
+    def build(self):
+        return RootWidget
+
+
+MainApp().run()

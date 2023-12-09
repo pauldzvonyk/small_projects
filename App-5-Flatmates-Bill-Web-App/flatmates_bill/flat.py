b8466f138097fb2d86d8d71e12d9316f914c949e
Pavlo Dzvonyk <reptile1601@yahoo.com>
2023-12-09 08:59:08 +0100

    templates and css for flatmates bill

--- App-5-Flatmates-Bill-Web-App/flatmates_bill/flat.py
+++ App-5-Flatmates-Bill-Web-App/flatmates_bill/flat.py
@@ -22,4 +22,4 @@ class Flatmate:
     def pays(self, bill, flatmate2):
         weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
         to_pay = bill.amount * weight
-        return to_pay
+        return round(to_pay, 2)

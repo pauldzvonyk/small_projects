76d7091ddc1f8d5b922cebaf6c9daea3b3e08184
Pavlo Dzvonyk <reptile1601@yahoo.com>
2023-12-27 14:12:18 +0100

    step 2 in merging app-8

--- App-8-Instant-Dictionary-Webapp/main.py
+++ App-8-Instant-Dictionary-Webapp/main.py
@@ -1,16 +1,12 @@
 import inspect
 import justpy as jp
 from webapp import page
-<<<<<<< HEAD
+
 # These imports are necessary for route registration, even though they are marked as unused
 from webapp.home import Home
 from webapp.about import About
 from webapp.dictionary import Dictionary
-=======
-# from webapp.home import Home
-# from webapp.about import About
-# from webapp.dictionary import Dictionary
->>>>>>> 81074191200efa6791b9623749110440855e40af
+
 
 imports = list(globals().values())
 for obj in imports:
